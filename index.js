aab87672c88275456ad550bd293f28a42f5be6f9
meterota <met2net3@gmail.com>
2020-05-25 22:13:50 +0300

    * v1.0.9

--- index.js
+++ index.js
@@ -1,27 +1,5 @@
 var validator = require("validator");
 
-const isEmptyString = ({ param, name, ERROR_TYPES }) => {
-    let resolve = false;
-    let error = null;
-    if(Array.isArray(param)) {
-        let subResolve = false;
-        for(let paramIndex = 0; paramIndex < param.length; paramIndex++) {
-            const isValid = param[paramIndex].trim() !== "";
-            subResolve = isValid;
-            if(!subResolve) break;
-        }
-        resolve = subResolve;
-        if(!resolve) error = name + ERROR_TYPES["isEmptyStringArray"];
-    } else {
-        resolve = param.trim() !== "";
-        if(!resolve) error = name + ERROR_TYPES["isEmptyString"];
-    }
-    return {
-        resolve,
-        error
-    };
-};
-
 const isHexColor = ({ param, name, ERROR_TYPES }) => {
     let resolve = false;
     let error = null;
